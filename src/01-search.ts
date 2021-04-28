d0124e3fa502cfafc5a61bb698c9689b81c503fa
Mathijs Beemsterboer <mathijs@filosofie.xyz>
2021-04-28 12:40:40 +0200

    Now you can search in all the directions, instead of only Y

--- src/01-search.ts
+++ src/01-search.ts
@@ -14,23 +14,27 @@ namespace search {
      * @param pPos position to start the search from
      * @param nSize the maximum height to search for (maxY is 255 in minecraft)
      * @param nBlockID the blockID to search
+     * @param dDir the direction in which to search
      * @returns 
      */
     // Code from: https://en.wikipedia.org/wiki/Exponential_search
-    export function exponential(pPos: Position, nSize: number, nBlockID: number): number {
+    export function exponential(pPos: Position, nSize: number, nBlockID: number, dDir: Axis = Axis.Y): number {
+        let x = 0, y = 0, z = 0;
+        dDir == Axis.X ? x = 1 : dDir == Axis.Y ? y = 1 : z = 1;
+
         // nBlockID is found at first Y position
-        if (blocks.testForBlock(nBlockID, positions.add(pPos, pos(0, 1, 0)))) {
-            return pPos.getValue(Axis.Y);
+        if (blocks.testForBlock(nBlockID, positions.add(pPos, pos(x, y, z)))) {
+            return pPos.getValue(dDir);
         } 
 
         // Find range for binary search by repeated doubling
-        let i = 1;
-        while (i < nSize && !blocks.testForBlock(nBlockID, positions.add(pPos, pos(0, i, 0)))) {
-            i *= 2;
+        while ((x < nSize || y < nSize || z < nSize) && !blocks.testForBlock(nBlockID, positions.add(pPos, pos(x, y, z)))) {
+            dDir == Axis.X ? x *= 2 : dDir == Axis.Z ? z *= 2 : y *= 2;
         }
         
         // Call binary search for the found range
-        return search.binary(pPos, i/2, Math.min(i, nSize - 1), nBlockID);
+        let i = x > y ? x : y > z ? y : z;
+        return search.binary(pPos, i/2, Math.min(i, nSize - 1), nBlockID, dDir);
     }
 
 
@@ -38,31 +42,52 @@ namespace search {
      * Binary search compares the middle block and the block beneath it, to 
      * determine if the block is to be found above or beneath it. 
      * @param pPos position to start the search from
-     * @param nDown the lower Y-value of the range
-     * @param nUp the upper Y-value of the range
+     * @param nLeft the left value of the range
+     * @param nRight the right value of the range
      * @param nBlockID the blockID to search
+     * @param dDir the direction in which to search
      * @returns 
      */
     // Code from: https://en.wikipedia.org/wiki/Binary_search_algorithm#Procedure
-    export function binary(pPos: Position, nDown: number, nUp: number, nBlockID: number): number {
-        if (nDown <= nUp) {
-            let nMid = Math.floor((nDown + nUp) / 2);
+    export function binary(pPos: Position, nLeft: number, nRight: number, nBlockID: number, dDir: Axis = Axis.Y): number {
+        if (nLeft <= nRight) {
+            let x = 0, y = 0, z = 0, midBlock, midNextBlock, nMid;
 
-            let midBlock = blocks.testForBlock(nBlockID, positions.add(pPos, pos(0, nMid, 0)));
-            let midLowerBlock = blocks.testForBlock(nBlockID, positions.add(pPos, pos(0, nMid-1, 0)))
+            // determine the middle of the range
+            dDir == Axis.X ? x = Math.floor((nLeft + nRight) / 2) : x = 0;
+            dDir == Axis.Y ? y = Math.floor((nLeft + nRight) / 2) : y = 0;
+            dDir == Axis.Z ? z = Math.floor((nLeft + nRight) / 2) : z = 0;
 
+            // check if midblock is the block to be found.
+            midBlock = blocks.testForBlock(nBlockID, positions.add(pPos, pos(x, y, z)));
+
+            // check what block is the next from the middle block. We need that to 
+            // determine if we found the top of the building/structure.
+            if (dDir == Axis.X) {
+                midNextBlock = blocks.testForBlock(nBlockID, positions.add(pPos, pos(x - 1, y, z)));
+                nMid = x;
+            } 
+            else if (dDir == Axis.Y) {
+                midNextBlock = blocks.testForBlock(nBlockID, positions.add(pPos, pos(x, y - 1, z)));
+                nMid = y;
+            }
+            else {
+                midNextBlock = blocks.testForBlock(nBlockID, positions.add(pPos, pos(x, y, z - 1)));
+                nMid = z
+            }
+            
             // If the block is found at the middle itself
-            if (midBlock && !midLowerBlock) {
+            if (midBlock && !midNextBlock) {
                 return nMid;
             }
 
             // If the block is both not found, the block should be found upwards
-            if(!midBlock && !midLowerBlock) {
-                return search.binary(pPos, nMid + 1, nUp, nBlockID);
+            if(!midBlock && !midNextBlock) {
+                return search.binary(pPos, nMid + 1, nRight, nBlockID, dDir);
             }
 
             // if the block is both found, the block should be found downwards
-            return search.binary(pPos, nDown, nMid - 1, nBlockID);
+            return search.binary(pPos, nLeft, nMid - 1, nBlockID, dDir);
         }
         console.error(`Block not found!`);
         return -1
