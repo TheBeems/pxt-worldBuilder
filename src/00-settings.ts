14aea583ebd12d4dde342376055879e5d90ce07f
TheBeems <mathijs@filosofie.xyz>
2021-05-05 11:37:08 +0200

    Bumped version after wall add fix

--- src/00-settings.ts
+++ src/00-settings.ts
@@ -2,7 +2,7 @@
  * 
  * Author:          TheBeems (Mathijs Beemsterboer)
  * Initial release: 2021-04-07
- * Last modified:   2021-05-03
+ * Last modified:   2021-05-05
  * Description:     Making building inside Minecraft:Education Edition a little easier.
  * 
  */
@@ -41,7 +41,7 @@
  * Class with the Data and settings.
  */
 class Data {
-    static sVersion: string = "1.5.1";
+    static sVersion: string = "1.5.2";
     static bDebug: boolean = true;
     static bShowMark: boolean = true;
     static aMarks: Position[] = [];
